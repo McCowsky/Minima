64192b4828e96b535c5d6d733f6d7da4839f7f22
McCowsky <makowskimateusz@outlook.com>
2022-09-02 02:12:50 +0200

    change paddings, units, typos

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,15 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        custom_orange: "#FF8811",
+      },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
         jakarta: ["jakarta"],
       },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(#FF8811 2px, transparent 0)",
+        "gradient-radial": "radial-gradient(custom_orange 2px, transparent 0)",
       },
       backgroundSize: {
         "back-size": "30px 30px",
@@ -18,10 +21,10 @@ module.exports = {
       "2xl": { max: "1650px" },
       // => @media (max-width: 1535px) { ... }
 
-      xl: { max: "1366px" },
+      xl: { max: "1400px" },
       // => @media (max-width: 1279px) { ... }
 
-      lg: { max: "1180px" },
+      lg: { max: "1230px" },
       // => @media (max-width: 1023px) { ... }
 
       md: { max: "767px" },
